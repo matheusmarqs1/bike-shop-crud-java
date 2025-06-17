25a221774390c519de4c2ffa97edaf34b450344c
Matheus Teles <matheusti770@gmail.com>
2025-06-17 12:27:45 -0300

    Implement CustomerDAO findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,38 +1,17 @@
 package application;
 
-import java.time.LocalDateTime;
-
 import model.dao.CustomerDao;
 import model.dao.DaoFactory;
 import model.entities.Customer;
-import model.entities.Order;
-import model.entities.OrderItem;
-import model.entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		 Customer customer = new Customer(1, "João", "Silva", "joao@example.com", "99999999", "Rua A");
-		 
-		 Order order = new Order(1, "001", "pending", LocalDateTime.now(), customer);
-		 
-		 customer.addOrder(order);
-		 
-		  Product product1 = new Product(1, "Notebook","a notebook","notebook",3000.0, 10);
-	      Product product2 = new Product(2, "Mouse", "a mouse", "mouse", 100.0, 50);
-	       
-	       OrderItem item1 = new OrderItem(1, product1, order, 2);
-	       order.addOrderItem(item1);
-	       OrderItem item2 = new OrderItem(2, product2, order, 1); 
-	       order.addOrderItem(item2);
-	       
-	       System.out.println(order);
-	       System.out.println(item1);
-	       System.out.println(item2);
-	       System.out.println(order.getTotalAmount());
-	       
+
 	       CustomerDao customerDao = DaoFactory.createCustomerDao();
+	       Customer customer = customerDao.findById(4);
+	       
+	       System.out.println(customer);
 	}
 
 }
--- src/model/dao/impl/CustomerDaoJDBC.java
+++ src/model/dao/impl/CustomerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.CustomerDao;
 import model.entities.Customer;
+import model.entities.Order;
 
 public class CustomerDaoJDBC implements CustomerDao {
+	
+	private Connection conn;
+	
+	public CustomerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Customer customer) {
@@ -27,8 +40,74 @@ public class CustomerDaoJDBC implements CustomerDao {
 
 	@Override
 	public Customer findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+							"SELECT " +
+				    	    "customers.id AS CustomerId, " +
+				    	    "customers.first_name, " +
+				    	    "customers.last_name, " +
+				    	    "customers.email, " +
+				    	    "customers.telephone, " +
+				    	    "customers.address, " +
+				    	    "orders.id AS OrderId, " +
+				    	    "orders.customer_id, " +
+				    	    "orders.order_number, " +
+				    	    "orders.total_amount, " +
+				    	    "orders.status, " +
+				    	    "orders.datetime " +
+				    	    "FROM customers JOIN orders ON customers.id = orders.customer_id " +
+				    	    "WHERE customers.id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			Customer customer = null;
+			
+			while(rs.next()) {
+				
+				if(customer == null) {
+					customer = instantiateCustomer(rs);
+				}
+				
+				Order order = instantiateOrder(rs, customer);
+				customer.addOrder(order);
+				
+			}
+			return customer;
+		
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	private Order instantiateOrder(ResultSet rs, Customer customer) throws SQLException {
+		
+		Order order = new Order();
+		order.setId(rs.getInt("OrderId"));
+		order.setOrderNumber(rs.getString("order_number"));
+		order.setStatus(rs.getString("status"));
+		order.setDatetime(rs.getTimestamp("datetime").toLocalDateTime());
+		order.setCustomer(customer);
+		return order;
+	}
+
+	private Customer instantiateCustomer(ResultSet rs) throws SQLException {
+		
+		Customer customer = new Customer();
+		customer.setId(rs.getInt("CustomerId"));
+		customer.setFirst_name(rs.getString("first_name"));
+		customer.setLast_name(rs.getString("last_name"));
+		customer.setEmail(rs.getString("email"));
+		customer.setTelephone(rs.getString("telephone"));
+		customer.setAddress(rs.getString("address"));
+		return customer;
 	}
 
 	@Override
