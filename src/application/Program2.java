e420d3f71b215c76c559d058d5a00b04f07d3715
Matheus Teles <matheusti770@gmail.com>
2025-07-01 10:31:55 -0300

    Implement ProductDao deleteById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.ProductDao;
@@ -11,6 +12,7 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		ProductDao productDao = DaoFactory.createProductDao();
 		
 		System.out.println("==== TEST 1: product findById ====");
@@ -25,13 +27,13 @@ public class Program2 {
 		for(Product p : list) {
 			System.out.println(p);
 		}
-		/*
+		
 		System.out.println("\n==== TEST 3: product insert ====");
 		
 		Product newProduct = new Product(null, "Bike Repair Kit", "Compact repair kit including tire levers, patch kit, multi-tool, and mini pump", "accessories", 34.99, 55);
 		productDao.insert(newProduct);
 		System.out.println("Inserted! New id = " + newProduct.getId());
-		*/
+		
 		
 		System.out.println("\n==== TEST 4: product update ====");
 		product = productDao.findById(10);
@@ -39,6 +41,14 @@ public class Program2 {
 		product.setPrice(75.00);
 		productDao.update(product);
 		System.out.println("Update completed! ");
+		
+		System.out.println("\n==== TEST 5: product deleteById ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		productDao.deleteById(id);
+		System.out.println("Delete completed! ");
+		
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/ProductDaoJDBC.java
+++ src/model/dao/impl/ProductDaoJDBC.java
@@ -86,7 +86,23 @@ public class ProductDaoJDBC implements ProductDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM products WHERE id = ?");
+			
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected == 0) {
+				throw new DbException("Delete failed: no record found with the specified ID");
+			}	
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
